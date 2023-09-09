a11310f91b15779f96b7bce73839e5bf3bae861e
Maria Claudia <mariaclaudia8.0@hotmail.com>
2023-09-09 20:03:25 -0300

    updates

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # config-debian-machine
+
 Scripts to configure my debian operational system because I use dual boot and Windows always break my linux. Also known as ricing. 
+
+In the main.sh, I install 
+
+- git
+- node js and npm
+- yarn 
+- nvm 
+
+Maybe in the future I'll have more scripts and more things to install
---
+++ main.sh
@@ -0,0 +1,19 @@
+#! /usr/bin/bash
+sudo apt update
+
+sudo apt install git nodejs npm curl
+
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+
+echo "deb https://dl.yarnpkg.com/debian/ stable main" |
+sudo tee /etc/apt/sources.list.d/yarn.list
+
+sudo apt install yarn 
+
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
+
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+
+
